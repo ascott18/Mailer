c79a69df995798a441e0f9ce0c304b90cef58061
Alex North <north.mister@gmail.com>
2015-05-26 13:28:21 -0700

    made unit test project

---
+++ LogicTesting/CloneTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mailer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTesting
+{
+    [TestClass]
+    public class CloneTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int id = 1;
+
+            Logic testClone = new Logic();
+
+            testClone.Clone(id);
+
+            using (var db = new MailerEntities())
+            {
+                var name = db.MailingLists.Single(ml => ml.ListID == id).Name;
+
+                var ids = db.MailingLists.Where(ml => ml.Name.Equals(name));
+
+            }
+
+        }
+    }
+}
