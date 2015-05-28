6472f6cbeca782b82179e6d663214ae481e6bcdc
Andrew Scott <ascott18@gmail.com>
2015-05-27 17:58:47 -0700

    Fixed the obnoxious xcopy build errors, and added a SampleMessageViewModel

---
+++ Mailer/DesignData/SampleAttachmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailer.DesignData
+{
+	class SampleAttachmentViewModel : AttachmentViewModel
+	{
+
+		public SampleAttachmentViewModel()
+		{
+			FileName = "CoolFile.png";
+		}
+	}
+}
