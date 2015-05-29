855aefa6b3590ed5c7387df0a82ffc00309e2cc0
Alex North <north.mister@gmail.com>
2015-05-29 14:59:17 -0700

    Added untracked file

---
+++ Mailer/DesignData/MockAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailer.DesignData
+{
+    class MockAddress : Address
+    {
+        
+        public MockAddress()
+        {
+            FirstName = "Frank";
+            LastName = "Sinatra";
+            Email = "franksinatra@dawg.net";
+
+        }
+    }
+}
