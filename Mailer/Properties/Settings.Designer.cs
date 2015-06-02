180d6e97654ddd8700cfaa60de57b50d5f4b6634
Andrew Scott <ascott18@gmail.com>
2015-06-01 19:13:22 -0700

    The database is now stored in %APPDATA%/Mailer/mailer.db, and created from a prototype if it doesnt exist

--- Mailer/Properties/Settings.Designer.cs
+++ Mailer/Properties/Settings.Designer.cs
@@ -23,16 +23,6 @@ namespace Mailer.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("data source=C:\\Users\\Andrew\\Desktop\\Mailer\\mailer.db")]
-        public string mailerConnectionString {
-            get {
-                return ((string)(this["mailerConnectionString"]));
-            }
-        }
-        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
