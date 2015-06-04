62bfd80bbceff8b9f4c88796f61603112361b4c7
Alex North <north.mister@gmail.com>
2015-06-04 13:13:19 -0700

    Recommited the stupid untracked files

---
+++ LogicTesting/EditMailingViewModelTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Mailer;
+using Mailer.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTesting
+{
+    [TestClass]
+    public class EditMailingViewModelTest
+    {
+        [TestMethod]
+        public void AddAddressIdTest()
+        {
+
+            var testAddress = new Address
+            {
+                Email = "asadsfasdfadsfdf@test.com",
+                FirstName = "yayheythere",
+                LastName = "demmetasdfadsfohyeah",
+                ReceivedMails = new List<ReceivedMail>()
+            };
+
+            var testList = new MailingList
+            {
+                Name = "heythere",
+                MailingListLines = new Collection<MailingListLine>()
+                   
+            };
+
+            using (var db = new MailerEntities())
+            {
+                db.MailingLists.Add(testList);
+                db.Addresses.Add(testAddress);
+              
+                db.SaveChanges();
+
+                var testEavm = new EditMailingViewModel(testList);
+
+                Assert.AreEqual(1, testEavm.AvailAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+
+                testEavm.AddAddressId(testAddress.AddressID);
+
+                Assert.AreEqual(1, testEavm.CurrAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+                Assert.AreEqual(0, testEavm.AvailAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+
+                db.MailingLists.Remove(testList);
+                db.SaveChanges();
+
+            }
+        }
+
+         [TestMethod]
+        public void RemoveAddressIdTest()
+        {
+
+            var testAddress = new Address
+            {
+                Email = "asadsfasdfadsfdf@test.com",
+                FirstName = "yayheythere",
+                LastName = "demmetasdfadsfohyeah",
+                ReceivedMails = new List<ReceivedMail>()
+            };
+
+            var testList = new MailingList
+            {
+                Name = "heythere",
+                MailingListLines = new Collection<MailingListLine>()
+                   
+            };
+
+            using (var db = new MailerEntities())
+            {
+                db.MailingLists.Add(testList);
+                db.Addresses.Add(testAddress);
+              
+                db.SaveChanges();
+
+                var testEavm = new EditMailingViewModel(testList);
+
+                testEavm.AddAddressId(testAddress.AddressID);
+
+
+                Assert.AreEqual(0, testEavm.AvailAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+
+                testEavm.RemoveAddressId(testAddress.AddressID);
+
+                Assert.AreEqual(0, testEavm.CurrAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+                Assert.AreEqual(1, testEavm.AvailAddresses.Count(addr => addr.AddressID == testAddress.AddressID));
+
+                db.MailingLists.Remove(testList);
+                db.SaveChanges();
+
+            }
+        }
+
+        [TestMethod]
+        public void SaveTest()
+        {
+            var testList = new MailingList
+            {
+                Name = "TestDBSave",
+            };
+
+            using (var db = new MailerEntities())
+            {
+                db.MailingLists.Add(testList);
+                db.SaveChanges();
+                var TestEavm = new EditMailingViewModel(testList);
+
+                TestEavm.Save();
+
+                Assert.IsTrue(db.MailingLists.Any(ml => testList.Name.Equals(ml.Name) && testList.ListID == ml.ListID));
+
+                db.MailingLists.Remove(testList);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ LogicTesting/MailingListViewModelTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Mailer;
+using Mailer.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTesting
+{
+    [TestClass]
+    public class MailingListViewModelTest
+    {
+        [TestMethod]
+        public void AddMailingListItemViewModelTest()
+        {
+            var mlvmTest = new MailingListViewModel();
+            
+
+            var testList = new MailingList
+            {
+                Name = "a",
+                MailingListLines = new Collection<MailingListLine>
+                   {
+                        new MailingListLine
+                        {
+                            Address = new Address
+                            {
+                                Email = "asdf@test.com",
+                                FirstName = "yay",
+                                LastName = "demmetasdfadsf",
+                                ReceivedMails = new List<ReceivedMail>()
+                            }
+                        }
+                    }
+            };
+
+            var mlivmTemp = new MailingListItemViewModel(testList, true);
+
+            mlvmTest.AddMailingListItemViewModel(mlivmTemp);
+            Assert.IsTrue(mlvmTest.MailingListItemViewModels.Contains(mlivmTemp));
+
+
+        }
+
+
+
+        [TestMethod]
+        public void MessagePumpTest()
+        {
+            var mlvmTest = new MailingListViewModel();
+
+            using (var db = new MailerEntities())
+            {
+                var mlist = new MailingList
+                {
+                    Name = "TestList"
+                };
+                db.MailingLists.Add(mlist);
+                db.SaveChanges();
+
+
+                var vm = new MailingListItemViewModel(mlist, true);
+                mlvmTest.AddMailingListItemViewModel(vm);
+                vm.Delete();
+
+                Assert.IsFalse(mlvmTest.MailingListItemViewModels.Contains(vm));
+
+            }
+        }
+    }
+}
